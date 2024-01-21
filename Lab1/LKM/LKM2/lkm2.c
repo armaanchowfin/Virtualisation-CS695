25da746b9052d51183cd25ca71b07b38e14ccf6c
armaanchowfin <armaanchowfin@gmail.com>
2024-01-21 16:40:55 +0530

    LKM2 v1

--- Lab1/LKM/LKM2/lkm2.c
+++ Lab1/LKM/LKM2/lkm2.c
@@ -8,19 +8,64 @@ Name the source file as lkm2.c
 #include <linux/module.h> 
 #include <linux/moduleparam.h> 
 #include <linux/printk.h> // printk aliases
+#include <linux/pid.h>
 
-static int mypid = 0;
+MODULE_DESCRIPTION("LKM2");
+MODULE_AUTHOR("Armaan Chowfin");
+MODULE_LICENSE("GPL");
 
-module_param(mypid, int, 0); 
-MODULE_PARM_DESC(mypid, "pid of desired process");
+static int p_id = 0;
+
+module_param(p_id, int, 0); 
+MODULE_PARM_DESC(p_id, "pid of desired process");
+
+
+/*
+ * /linux/list.h provides the Doubly-Linked Circular list API.
+ * list_entry: gets surrounding struct from its list_head
+*/
+static void get_child_processes(struct task_struct *base_task){
+    list_for_each_entry(base_task, &base_task->children, sibling){
+        pr_info("[%d]:[%s]\n", child_task->pid, child_task->comm);
+        get_child_processes(child_task);
+    }
+
+    // for(struct list_head *child_list_ent = child_head->next;
+    //     !list_is_head(child_head, child_list_ent);     
+    //     child_list_ent = child_list_ent->next           
+    //     ){
+    //         /**
+    //           * list_entry - get the struct for this entry
+    //           * list_entry(ptr, type, member)
+    //           * @ptr:	the &struct list_head pointer.
+    //           * @type:	the type of the struct this is embedded in.
+    //           * @member:	the name of the list_head within the struct.
+    //         */
+    //         struct task_struct *child_task = &list_entry(&child_list_ent, struct task_struct, sibling)
+    //         pr_info("[%d]:[%s]\n", child_task->pid, child_task->comm);
+            
+    //     }
+
+}
 
 static int __init lkm2_init(void){
+    //struct pid from pid integer.
+    struct pid *base_pid = find_get_pid(p_id); 
+
+    // Gives a reference to the task_struct of the process at pid p_id
+    struct task_struct *base_task = pid_task(base_pid, PIDTYPE_PID); 
+
+    get_child_processes(base_task);
+    
+    return 0;
 
 }
 
-static void __exit lkm1_exit(void){
+static void __exit lkm2_exit(void){
+
+    pr_info("LMK2 Completed");
 
 }
 
-module_init(lkm1_init);
-module_exit(lkm1_exit);
+module_init(lkm2_init);
+module_exit(lkm2_exit);
--- Lab1/LKM/LKM2/readme.md
+++ Lab1/LKM/LKM2/readme.md
@@ -4,10 +4,17 @@ Write a kernel module that takes process ID as input,
 and for each of its child processes, print their pid and process state. 
 Name the source file as lkm2.c
 ```
-1. Find out how to make command-line arguments available to the source-file 
+High-Level goal: Need to traverse the kernel process-tree.
+Approach 1: DFS -> from &init_task.
+1. Find out how to make command-line arguments available to the source-file -> via `module_param()`
 2. Find out how process-tree is organised.
 Refer: https://myaut.github.io/dtrace-stap-book/kernel/proc.html
+Q1. How are kernel threads and process different according to kernel?
 3. Create module
 3.1. How to obtain `task_struct` given a pid?
+->`pid2task()`
 Q1. Can a Kernel module by dynamically loaded by another module? (for usage as functions)
 A: 
+
+Approach 2: Refer pstree implementation.
+
